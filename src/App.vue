59d5600e9fd01806abc450c95030e39cf98a20e3
Владислав Барсков <v.a.barskov@yandex.ru>
2026-05-25 09:57:42 +0300

    first homework

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
-    <AppHeader />
-    <div class="main-page" v-if="showMain">
-        <AppButton>Начать игру</AppButton>
-    </div>
-    <div class="inner-page" v-else>
-        <div class="cards-wrapper">
-            <AppCard @rotated="console.log('rotated')" @completed="console.log('completed')" />
-        </div>
-    </div>
+    {{ date }}
 </template>
 
 <script setup>
-    import AppButton from './components/Button.vue'
-    import AppHeader from './components/Header.vue'
-    import AppCard from './components/Card.vue'
-    import {ref} from "vue";
+    import { ref } from "vue";
 
-    const showMain = ref(false);
+    const date = ref(new Date());
 </script>
 
 <style scoped>
-    .main-page {
-        display: grid;
-        place-items: center;
-        align-content: center;
-        min-height: 100vh;
-    }
 </style>
