1d35ac148665884ef8a0d327b019bf7fa60e2962
Владислав Барсков <v.a.barskov@yandex.ru>
2026-05-25 09:25:27 +0300

    header and card added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-    <div class="main-page">
+    <AppHeader />
+    <div class="main-page" v-if="showMain">
         <AppButton>Начать игру</AppButton>
+    </div>
+    <div class="inner-page" v-else>
+        <div class="cards-wrapper">
+            <AppCard />
+        </div>
     </div>
 </template>
 
 <script setup>
     import AppButton from './components/Button.vue'
+    import AppHeader from './components/Header.vue'
+    import AppCard from './components/Card.vue'
+    import {ref} from "vue";
+
+    const showMain = ref(false);
 </script>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="header">
+        <div class="title">
+            {{ title }}
+        </div>
+        <div class="score-wrapper">
+            <div class="score">
+                {{ score }}
+            </div>
+            <AppIconHeart />
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import AppIconHeart from './Icons/Heart.vue';
+    const { title, score } = defineProps({
+        title: {type: String, default: 'Запомни слово'},
+        score: {type: Number, default: 0},
+    });
+</script>
+
+<style scoped>
+    .header {
+        padding: 49px 62px 49px 66px;
+        display: flex;
+        justify-content: space-between;
+        font-size: var(--small-font-size);
+        line-height: var(--small-line-height);
+        font-weight: var(--bold-font-weight);
+        text-transform: uppercase;
+        align-items: center;
+    }
+    .title {
+        letter-spacing: 12%;
+    }
+    .score-wrapper {
+        padding: 12px 24px;
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #CCE8FF;
+        color: #008BFE;
+        border-radius: 36px;
+        border: none;
+    }
+</style>
